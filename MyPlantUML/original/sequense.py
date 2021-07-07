8711a70a9efd703950cd4eaca91caeda1f2802f6
sakuradish <ktok.hobby@gmail.com>
2021-07-08 07:01:46 +0900

    update sequence render

--- MyPlantUML/original/sequense.py
+++ MyPlantUML/original/sequense.py
@@ -15,130 +15,162 @@ from MyLoggerIf import *
 # import traceback
 ################################################################################
 class sequenceManager:
+    stack = []
     ################################################################################
-    ## @brief 初期化処理
-    @showTrace
-    def __init__(self):
-        self.stack = []
-        pass
-    ################################################################################
-    ## @brief インスタンス取得
     @classmethod
     @showTrace
-    def GetInstance(cls):
-        if not hasattr(cls, 'this_'):
-            cls.this_ = cls()
-        return cls.this_
+    def push(cls, from_entity, to_entity, message):
+        cls.stack.append({"from":from_entity.getName(), "to":to_entity.getName(), "message":message})
     ################################################################################
-    ## @brief インスタンス取得
-    @showTrace
-    def push(self, from_entity, to_entity, message):
-        for k,v in globals().items():
-            if id(v) == id(from_entity):
-                from_entity = k
-        for k,v in globals().items():
-            if id(v) == id(to_entity):
-                to_entity = k
-        self.stack.append({"from":from_entity, "to":to_entity, "message":message})
-    ################################################################################
-    ## @brief 初期化処理
+    @classmethod
     @showTrace
     def draw(self):
         ################################################################################
-        ## @brief 初期化処理
-        @showTrace
-        def drawBase():
-            entities = {}
-            for stack in self.stack:
-                entities[stack["from"]] = {}
-                entities[stack["to"]] = {}
-            sakura(entities)
-
-            maxentitynamelength = 0
-            for entity in entities.keys():
-                if maxentitynamelength < len(entity):
-                    maxentitynamelength = len(entity)
-            sakura(maxentitynamelength)
-
-            maxmessagelength = 0
-            for stack in self.stack:
-                if maxmessagelength < len(stack["message"]):
-                    maxmessagelength = len(stack["message"])
-            sakura(maxmessagelength)
-            
-            entity_w = 40 + maxentitynamelength * 5
-            entity_space = 60 + maxmessagelength * 5
-            horizontalline_y1 = 40
-            horizontalline_y2 = 70 + len(self.stack) * 30
-
-            
-                
-
-            # 縦横・間隔等をstackから出力
-            text = ""
-            text += '<line style="stroke:#A80036;stroke-width:1.0;stroke-dasharray:5.0,5.0;" x1="28" x2="28" y1="' + str(horizontalline_y1) + '" y2="' + str(horizontalline_y2) + '"/>' + "\n"
-            text += '<line style="stroke:#A80036;stroke-width:1.0;stroke-dasharray:5.0,5.0;" x1="88" x2="88" y1="' + str(horizontalline_y1) + '" y2="' + str(horizontalline_y2) + '"/>' + "\n"
-            
-            text += '<rect fill="#FEFECE" filter="url(#f1ovyoxgkqkxtz)" height="30.2969" style="stroke:#A80036;stroke-width:1.5;" width="43" x="5" y="5"/>' + "\n"
-            text += '<text fill="#000000" font-family="sans-serif" font-size="14" lengthAdjust="spacing" textLength="29" x="12" y="24.9951">Bob' + "\n"
-            text += '</text>' + "\n"
-            text += '<rect fill="#FEFECE" filter="url(#f1ovyoxgkqkxtz)" height="30.2969" style="stroke:#A80036;stroke-width:1.5;" width="43" x="5" y="88.4297"/>' + "\n"
-            text += '<text fill="#000000" font-family="sans-serif" font-size="14" lengthAdjust="spacing" textLength="29" x="12" y="108.4248">Bob' + "\n"
-            text += '</text>' + "\n"
-
-            text += '<rect fill="#FEFECE" filter="url(#f1ovyoxgkqkxtz)" height="30.2969" style="stroke:#A80036;stroke-width:1.5;" width="49" x="62" y="5"/>' + "\n"
-            text += '<text fill="#000000" font-family="sans-serif" font-size="14" lengthAdjust="spacing" textLength="35" x="69" y="24.9951">Alice' + "\n"
-            text += '</text>' + "\n"
-            text += '<rect fill="#FEFECE" filter="url(#f1ovyoxgkqkxtz)" height="30.2969" style="stroke:#A80036;stroke-width:1.5;" width="49" x="62" y="88.4297"/>' + "\n"
-            text += '<text fill="#000000" font-family="sans-serif" font-size="14" lengthAdjust="spacing" textLength="35" x="69" y="108.4248">Alice' + "\n"
-            text += '</text>' + "\n"
-            return text
+        def getHeader(w,h):
+            return \
+            '<?xml version="1.0" encoding="UTF-8" standalone="no"?> \n\
+            <svg xmlns="http://www.w3.org/2000/svg" \n\
+                 xmlns:xlink="http://www.w3.org/1999/xlink" \n\
+                 contentScriptType="application/ecmascript" \n\
+                 contentStyleType="text/css" \n\
+                 height="'+str(h)+'px" \n\
+                 preserveAspectRatio="none" \n\
+                 style="width:'+str(w)+'px;height:'+str(h)+'px;background:#FFFFFF;" \n\
+                 version="1.1" \n\
+                 viewBox="0 0 '+str(w)+' '+str(h)+'" \n\
+                 width="'+str(w)+'px" \n\
+                 zoomAndPan="magnify"> \n\
+            <defs> \n\
+            <filter height="300%" id="f1ovyoxgkqkxtz" width="300%" x="-1" y="-1"> \n\
+            <feGaussianBlur result="blurOut" stdDeviation="2.0"/> \n\
+            <feColorMatrix in="blurOut" result="blurOut2" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .4 0"/> \n\
+            <feOffset dx="4.0" dy="4.0" in="blurOut2" result="blurOut3"/> \n\
+            <feBlend in="SourceGraphic" in2="blurOut3" mode="normal"/> \n\
+            </filter> \n\
+            </defs> \n\
+            <g>' + '\n'
         ################################################################################
-        ## @brief 初期化処理
-        @showTrace
-        def drawMessage():
-            text = ""
-            text += '<polygon fill="#A80036" points="76.5,67.4297,86.5,71.4297,76.5,75.4297,80.5,71.4297" style="stroke:#A80036;stroke-width:1.0;"/>' + "\n"
-            text += '<line style="stroke:#A80036;stroke-width:1.0;" x1="28.5" x2="82.5" y1="71.4297" y2="71.4297"/>' + "\n"
-            text += '<text fill="#000000" font-family="sans-serif" font-size="13" lengthAdjust="spacing" textLength="30" x="35.5" y="66.3638">hello' + "\n"
-            text += '</text>' + "\n"
-            return text
+        def getFooter():
+            return \
+            '</g> \n\
+            </svg>' + '\n'
         ################################################################################
+        def drawLine(x1,y1,x2,y2):
+            return \
+            '<line \n\
+            style="stroke:#A80036;stroke-width:1.0;stroke-dasharray:5.0,5.0;" \n\
+            x1="'+str(x1)+'" \n\
+            x2="'+str(x2)+'" \n\
+            y1="'+str(y1)+'" \n\
+            y2="'+str(y2)+'" \n\
+            />' + "\n"
+        ################################################################################
+        def drawArrow(x1,y1,x2,y2,direction="TORIGHT"):
+            if direction=="TORIGHT":
+                return \
+                '<polygon \n\
+                fill="#A80036" \n\
+                points="\n\
+                '+str(x2-7)+','+str(y2-7)+',\n\
+                '+str(x2-3)+','+str(y2)+',\n\
+                '+str(x2-7)+','+str(y2+7)+',\n\
+                '+str(x2)+','+str(y2)+'" \n\
+                style="stroke:#A80036;stroke-width:1.0;" \n\
+                />' + "\n" + drawLine(x1,x2,y1,y2)
+            elif direction=="TOLEFT":
+                return \
+                '<polygon \n\
+                fill="#A80036" \n\
+                points="\n\
+                '+str(x1+7)+','+str(y1+7)+',\n\
+                '+str(x1+3)+','+str(y1)+',\n\
+                '+str(x1+7)+','+str(y1-7)+',\n\
+                '+str(x1)+','+str(y1)+'" \n\
+                style="stroke:#A80036;stroke-width:1.0;" \n\
+                />' + "\n" + drawLine(x1,x2,y1,y2)
+            else:
+                critical("NOT SUPPORTED")
+        ################################################################################
+        def drawRect(x,y,w,h):
+            return \
+            '<rect \n\
+            fill="#FEFECE" \n\
+            filter="url(#f1ovyoxgkqkxtz)" \n\
+            height="'+str(h)+'" \n\
+            style="stroke:#A80036;stroke-width:1.5;" \n\
+            width="'+str(w)+'" \n\
+            x="'+str(x)+'" \n\
+            y="'+str(y)+'" \n\
+            />' + "\n"
+        ################################################################################
+        def drawText(x,y,w,message):
+            return \
+            '<text \n\
+            fill="#000000" \n\
+            font-family="sans-serif" \n\
+            font-size="14" \n\
+            lengthAdjust="spacing" \n\
+            textLength="'+str(w)+'" \n\
+            x="'+str(x)+'" \n\
+            y="'+str(y)+'"\n\
+            >'+str(message)+'</text>' + "\n"
+        ################################################################################
+        entities = {}
+        for stack in self.stack:
+            entities[stack["from"]] = {}
+            entities[stack["to"]] = {}
+        maxEntityNameLength = max([len(entity) for entity in entities.keys()])
+        maxMessageLength = max([len(stack["message"]) for stack in self.stack])
+        # ベースとなる縦線とオブジェクト達のレイアウトを計算
+        arrowLength_v = 20 + (maxMessageLength * 10)
+        arrowLength_h = 20 + (len(self.stack) * 20)
+        for entity in entities.keys():
+            entities[entity]["top_x"] = 10 + list(entities.keys()).index(entity) * arrowLength_v
+            entities[entity]["top_y"] = 10
+            entities[entity]["top_w"] = 20 + (maxEntityNameLength * 10)
+            entities[entity]["top_h"] = 30
+            entities[entity]["bottom_x"] = entities[entity]["top_x"]
+            entities[entity]["bottom_y"] = entities[entity]["top_y"] + entities[entity]["top_h"] + arrowLength_h
+            entities[entity]["bottom_w"] = entities[entity]["top_w"]
+            entities[entity]["bottom_h"] = entities[entity]["top_h"]
+
         text = ""
-        text += '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + "\n"
-        text += '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" contentScriptType="application/ecmascript" contentStyleType="text/css" height="172px" preserveAspectRatio="none" style="width:163px;height:172px;background:#FFFFFF;" version="1.1" viewBox="0 0 163 172" width="163px" zoomAndPan="magnify">' + "\n"
-        text += '<defs>' + "\n"
-        text += '<filter height="300%" id="f1ovyoxgkqkxtz" width="300%" x="-1" y="-1">' + "\n"
-        text += '<feGaussianBlur result="blurOut" stdDeviation="2.0"/>' + "\n"
-        text += '<feColorMatrix in="blurOut" result="blurOut2" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 .4 0"/>' + "\n"
-        text += '<feOffset dx="4.0" dy="4.0" in="blurOut2" result="blurOut3"/>' + "\n"
-        text += '<feBlend in="SourceGraphic" in2="blurOut3" mode="normal"/>' + "\n"
-        text += '</filter>' + "\n"
-        text += '</defs>' + "\n"
-        text += '<g>' + "\n"
-        text += drawBase()
-        text += drawMessage()
-        text += '</g>' + "\n"
-        text += '</svg>' + "\n"
+        text += getHeader(1000,1000)
+        for entity,params in entities.items():
+            text += drawLine(params["top_x"]+(params["top_w"]/2),\
+                             params["top_y"]+params["top_h"],\
+                             params["bottom_x"]+(params["bottom_w"]/2),\
+                             params["bottom_y"])
+            text += drawRect(params["top_x"],params["top_y"],params["top_w"],params["top_h"])
+            text += drawText(params["top_x"]+10,params["top_y"]+(params["top_h"]/2),(maxEntityNameLength * 10),entity)
+            text += drawRect(params["bottom_x"],params["bottom_y"],params["bottom_w"],params["bottom_h"])
+            text += drawText(params["bottom_x"]+10,params["bottom_y"]+(params["bottom_h"]/2),(maxEntityNameLength * 10),entity)
+        for stack in self.stack:
+            # TODO : drawing communication message
+            sakura(self.stack.index(stack),stack)
+        text += getFooter()
+        ################################################################################
         open("output.svg", "w", encoding="utf-8").write(text)
 ################################################################################
 ## @brief sequence作図クラス
 class entity:
     ################################################################################
-    ## @brief 初期化処理
     @showTrace
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
     ################################################################################
-    ## @brief 初期化処理
     @showTrace
     def call(self, entity, message):
-        sequenceManager.GetInstance().push(self, entity, message)
+        sequenceManager.push(self, entity, message)
+    ################################################################################
+    @showTrace
+    def getName(self):
+        return self.name
 ###############################################################################
 if __name__ == '__main__':
-    Bob = entity()
-    Alice = entity()
+    Bob = entity("Bob")
+    Alice = entity("Alice")
     Bob.call(Alice, "Hello")
     Bob.call(Alice, "GoodBye")
-    sequenceManager.GetInstance().draw()
+    sequenceManager.draw()
 ###############################################################################
